b03301b2e6144eb1ba92866ceb26ccc9276dfb59
Phutanate Pisutsin <phutanate.pisutsin@mail.utoronto.ca>
2024-12-25 22:54:12 +0700

    fix typo

--- app/helper/page.js
+++ app/helper/page.js
@@ -142,7 +142,7 @@ export default function Helper() {
               type="submit"
               className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500"
             >
-              Create Account
+              Submit
             </button>
           </form>
         </div>
